f0e85f2caf67350dd37326e7fdc05001c0c1f537
b-maldoca <66144360+b-maldoca@users.noreply.github.com>
2021-09-14 18:54:08 +0200

    adding kokoro builds for windows (#65)

---
+++ ci/kokoro/gcp_windows/cc_coverage/cc_coverage.bat
@@ -0,0 +1,26 @@
+rem Copyright 2021 Google LLC
+rem
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem     https://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+
+rem This script should be run at the root of maldoca repo.
+choco install -y bazel
+choco install -y llvm
+
+rem seems to break the kokoro automation so removing it
+rem refreshenv
+
+git submodule update --init --recursive
+
+rem Run MalDocA coverage.
+bazel coverage --config=windows maldoca/... --test_output=errors --verbose_failures
+
---
+++ ci/kokoro/gcp_windows/cc_test/kokoro_build.bat
@@ -0,0 +1,20 @@
+rem Copyright 2021 Google LLC
+rem
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem     https://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+
+cd %KOKORO_ARTIFACTS_DIR%\github\maldoca
+
+call ci\kokoro\gcp_windows\cc_test\cc_test.bat
+
+exit /b %ERRORLEVEL%
+
